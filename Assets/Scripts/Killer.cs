914b1e7e536757cf74e8c82faf31e196c77076d9
David Portisch <eisdrache12@hotmail.com>
2014-01-25 16:30:22 +0100

    Improvements in Killscripts

--- Assets/Scripts/Killer.cs
+++ Assets/Scripts/Killer.cs
@@ -16,17 +16,24 @@ public class Killer : MonoBehaviour {
 	
 	public void kill() {
 		var gameObjects = FindObjectsOfType(typeof(GameObject)) as GameObject[];
+		DamageTaker nearestDt = null;
+		float nearestDistance = Mathf.Pow(radius, 2);
 		
 		foreach(GameObject go in gameObjects) {
 			DamageTaker dt = go.GetComponent<DamageTaker>();
-			if(dt != null) {
+			if(dt != null && !go.Equals(this.gameObject)) {
 				float squareDistance = Mathf.Pow(go.transform.position.x - transform.position.x, 2)
 					+ Mathf.Pow(go.transform.position.z - transform.position.z, 2);
-				Debug.Log(squareDistance);
-				if(squareDistance < Mathf.Pow(radius, 2)) {
-					dt.takeDamage(this.gameObject);
+				
+				if(squareDistance <= nearestDistance) {
+					nearestDt = dt;
+					nearestDistance = squareDistance;
 				}
 			}
+		}
+		
+		if(nearestDt != null) {
+			nearestDt.takeDamage(this.gameObject);
 		}
 	}
 	

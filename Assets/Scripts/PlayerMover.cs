e19bdca7425bbdbc3738b01ca4b997e1efb1438f
meredrica <stuff@meredrica.org>
2014-01-25 15:01:39 +0100

    fix controller dpad input

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -4,7 +4,7 @@ using System.Collections;
 
 public interface InputWrapper {
 	float getXMod();
-	float getYMod();
+	float getZMod();
 	bool murder();
 }
 
@@ -19,7 +19,7 @@ public class Keyboard1Controls : InputWrapper {
 		return 0;
 	}
 	
-	public float getYMod() {
+	public float getZMod() {
 		if (Input.GetKey (KeyCode.UpArrow)) {
 			return -1;
 		} else if (Input.GetKey(KeyCode.PageDown)) {
@@ -44,36 +44,43 @@ public class GamepadControls : InputWrapper {
 	public float getXMod() {
 		var state = GamePad.GetState(index);
 		
-		if(Mathf.Abs (state.ThumbSticks.Left.X) > 0) {
+		if (Mathf.Abs (state.ThumbSticks.Left.X) > 0) {
 			return state.ThumbSticks.Left.X;
-		} else if((bool)state.DPad.Left) {
-			return -1;
-		} else if((bool)state.DPad.Right) {
-			return 1;
+			// TODO: wrap to [-1,1]
+		} else {
+			if(state.DPad.Left.Equals(ButtonState.Pressed)) {
+				return -1;
+			}
+			if(state.DPad.Right.Equals(ButtonState.Pressed)) {
+				return 1;
+			}
 		}
-		
 		return 0;
 	}
 	
-	public float getYMod() {
+	public float getZMod() {
 		var state = GamePad.GetState(index);
 		
-		if(Mathf.Abs (state.ThumbSticks.Left.Y) > 0) {
+		if (Mathf.Abs (state.ThumbSticks.Left.Y) > 0) {
 			return state.ThumbSticks.Left.X;
-		} else if((bool)state.DPad.Down) {
-			return -1;
-		} else if((bool)state.DPad.Up) {
-			return 1;
+			// TODO: wrap to [-1,1]
+		} else {
+			if(state.DPad.Down.Equals(ButtonState.Pressed)) {
+				return -1;
+			}
+			if(state.DPad.Up.Equals(ButtonState.Pressed)) {
+				return 1;
+			}
 		}
-		
 		return 0;
 	}
 	
 	public bool murder() {
 		var state = GamePad.GetState(index);
-		
+		return true;
+		/*
 		return state.Buttons.X;
-	}
+	*/}
 }
 
 public class Keyboard2Controls : InputWrapper {
@@ -87,7 +94,7 @@ public class Keyboard2Controls : InputWrapper {
 		return 0;
 	}
 	
-	public float getYMod() {
+	public float getZMod() {
 		if (Input.GetKey (KeyCode.S)) {
 			return -1;
 		} else if (Input.GetKey(KeyCode.W)) {
@@ -119,6 +126,7 @@ public class PlayerMover : MonoBehaviour {
 		if(wrapper == null) {
 			if(controllerPlayer > -1) {
 				wrapper = new GamepadControls(controllerPlayer);
+
 			} else if(keyboardPlayer == 0) {
 				wrapper = new Keyboard1Controls();
 			} else if(keyboardPlayer == 1) {
@@ -130,12 +138,15 @@ public class PlayerMover : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		checkWrapper();
-		
+	
 		float delta = Time.deltaTime;
 		
+		
 		if(wrapper != null) {
-			//TODO use move script
-			transform.Translate (wrapper.getXMod() * speed * delta,0 , wrapper.getYMod() * speed * delta);
+			Debug.Log("getX: "+wrapper.getXMod());
+			Debug.Log("getZ: "+wrapper.getZMod());
+					//TODO use move script
+				transform.Translate (wrapper.getXMod() * speed * delta,0 , wrapper.getZMod() * speed * delta);
 			
 			if(wrapper.murder()) {
 				//TODO Send murder message

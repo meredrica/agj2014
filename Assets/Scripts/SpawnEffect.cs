c39281d35dae42f0876feb181595e5a58f9942b8
David Portisch <eisdrache12@hotmail.com>
2014-01-25 22:52:55 +0100

    Added a spawn effect

---
+++ Assets/Scripts/SpawnEffect.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnEffect : MonoBehaviour {
+	public float mSpawnTime = 1;
+
+	// Use this for initialization
+	void Start () {
+		startEffect();
+	}
+	
+	public void startEffect() {
+		transform.localScale = new Vector3(0, 0, 0);
+		StartCoroutine("Fade");
+	}
+	
+	void update() {
+	}
+	
+	IEnumerator Fade() {
+		for(float f = 0; f < mSpawnTime; f += Time.deltaTime) {
+			transform.localScale = new Vector3(f/mSpawnTime, f/mSpawnTime, f/mSpawnTime);
+			yield return null;
+		}
+		
+		transform.localScale = new Vector3(1, 1, 1);
+	}
+}

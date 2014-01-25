d071849ec24f08ead0e1b44fe559ba3b83a8d450
David Portisch <eisdrache12@hotmail.com>
2014-01-25 18:09:55 +0100

    Even better killing

--- Assets/Scripts/Killer.cs
+++ Assets/Scripts/Killer.cs
@@ -21,7 +21,7 @@ public class Killer : MonoBehaviour {
 		
 		foreach(GameObject go in gameObjects) {
 			DamageTaker dt = go.GetComponent<DamageTaker>();
-			if(dt != null && !go.Equals(this.gameObject)) {
+			if(dt != null && !go.Equals(this.gameObject) && go.GetComponent<DamageTaker>().isAlive()) {
 				float squareDistance = Mathf.Pow(go.transform.position.x - transform.position.x, 2)
 					+ Mathf.Pow(go.transform.position.z - transform.position.z, 2);
 				
--- Assets/Scripts/NPCDamageTaker.cs
+++ Assets/Scripts/NPCDamageTaker.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 using System.Collections;
 
 public class NPCDamageTaker : DamageTaker {
-	public override void takeDamage (GameObject killer) {
-		//Play sounds and do other cool stuff
-		Destroy(this.gameObject);
+	protected override void rewardPoints ()
+	{
+		//Implement negative points
 	}
 }
